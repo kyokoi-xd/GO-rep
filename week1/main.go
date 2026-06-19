4a3cf43145e83f8a11a794498bc12e7d8336c040
kyokoi-xd <danilich.huilich@gmail.com>
2026-06-19 15:12:05 +0300

    Add initial Go module and main program file

---
+++ week1/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, Go!")
+	name := "Dmitry"
+	fmt.Println(name)
+	age := 25
+	fmt.Println(age)
+	city := "New York"
+	fmt.Println(city)
+}
