5f00aa42ec350c6daddc972ddbefef3c4a2c39c2
kyokoi-xd <danilich.huilich@gmail.com>
2026-06-22 16:04:34 +0300

    Refactor main function to display user details and future age calculation

--- week1/main.go
+++ week1/main.go
@@ -3,11 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, Go!")
-	name := "Dmitry"
-	fmt.Println(name)
-	age := 25
-	fmt.Println(age)
-	city := "New York"
-	fmt.Println(city)
+	name := "Alice"
+	age := 30
+	salary := 50000.016
+	student := true
+
+	fmt.Printf("\nName: %s\n", name)
+	fmt.Printf("Age: %d\n", age)
+	fmt.Printf("Salary: %.2f\n", salary)
+	fmt.Printf("Is Student: %t\n", student)
+	fmt.Printf("After 5 years, %s will be %d years old.\n", name, age+5)
 }
