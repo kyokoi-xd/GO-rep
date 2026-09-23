c614c11bd5ec4495312964abd5900c34e6e20084
ahahahahah <danilich.huilich@gmail.com>
2026-09-23 18:57:27 +0300

    Add arithmetic operations for addition, subtraction, multiplication, and division

--- day8/homework.go
+++ day8/homework.go
@@ -42,4 +42,56 @@ func main() {
 	case 7:
 		fmt.Println("Sunday")
 	}
+
+	var firstNum, secondNum, operation int
+	fmt.Println("Enter first number: ")
+	fmt.Scan(&firstNum)
+	fmt.Println("Enter second number: ")
+	fmt.Scan(&secondNum)
+
+	fmt.Println("Choose operation:")
+	fmt.Println("1. Add")
+	fmt.Println("2. Subtract")
+	fmt.Println("3. Multiply")
+	fmt.Println("4. Divide")
+
+	fmt.Println("Enter number of operation: ")
+	fmt.Scan(&operation)
+
+	switch operation {
+	case 1:
+		fmt.Println("Result:", add(firstNum, secondNum))
+	case 2:
+		fmt.Println("Result:", subtract(firstNum, secondNum))
+	case 3:
+		fmt.Println("Result:", multiply(firstNum, secondNum))
+	case 4:
+		result, err := divide(firstNum, secondNum)
+		if err != nil {
+			fmt.Println("Error:", err)
+		} else {
+			fmt.Println("Result:", result)
+		}
+	default:
+		fmt.Println("Unknown operation")
+	}
+}
+
+func add(a int, b int) int {
+	return a + b
+}
+
+func subtract(a int, b int) int {
+	return a - b
+}
+
+func multiply(a int, b int) int {
+	return a * b
+}
+
+func divide(a int, b int) (int, error) {
+	if b == 0 {
+		return 0, fmt.Errorf("division by zero")
+	}
+	return a / b, nil
 }
